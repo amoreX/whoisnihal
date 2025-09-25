d8bef5b4e0cbc8855a3c2b7f33d689f24ebae0c8
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-09-25 23:13:54 +0530

    Add active flag for controls

--- src/components/header/Controls.tsx
+++ src/components/header/Controls.tsx
@@ -1,15 +1,19 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
-const Control = ({children} : {children: ReactNode}) => {
-    return <div className='flex items-center hover:text-accent cursor-pointer py-4 justify-center'>{children}</div>
+const Control = ({children, active} : {children: ReactNode, active:boolean}) => {
+    return <div className={`flex items-center  cursor-pointer py-4 justify-center ${active ? "text-accent" : "text-stone-400 hover:text-accent"}`}>{children}</div>
 }
 
 const Controls = () => {
+    const pathname = usePathname()
+    
   return (
     <div className='flex gap-6 sticky top-0 bg-background z-[100] h-fit items-stretch select-none  text-lg'>
-        <Control>[h] Home</Control>
-        <Control>[p] Projects</Control>
-        <Control>[b] Blog</Control>
+        <Control active={pathname === "/"}>[h] Home</Control>
+        <Control active={pathname?.startsWith("/project")}>[p] Projects</Control>
+        <Control active={pathname?.startsWith("/blog")}>[b] Blog</Control>
         <div className='absolute bg-gradient-to-b from-background to-transparent pointer-events-none size-full top-full left-0 ' ></div>
     </div>
   )
