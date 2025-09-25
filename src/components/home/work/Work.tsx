fa85982c2c435c0e43d3b7e094a77d18d14ba2ac
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-09-25 23:38:18 +0530

    Fix outlines

--- src/components/home/work/Work.tsx
+++ src/components/home/work/Work.tsx
@@ -8,14 +8,14 @@ const Work = ({company, title, date, description} : {
     description: string
 }) => {
   return (
-    <div className='w-full relative  flex flex-col group cursor-pointer hover:bg-stone-900/40 '>
+    <button className='w-full relative items-start text-left flex flex-col group cursor-pointer hover:bg-stone-900/40 '>
         <div className='absolute group-hover:opacity-100 opacity-0 top-0 right-0 size-[32px] bg-stone-700 flex items-center justify-center' >
             <ArrowUpRightIcon size={20}/>
         </div>
         <div className='text-xl text-white '>{company}</div>
         <div className='text-stone-400 font-medium text-sm   flex gap-2 items-center' >{title} <div className='w-[1px] h-[10px] bg-stone-500 translate-y-[2px]' ></div> {date}</div>
         <div className='text-stone-300 mt-1' >{description}</div>
-    </div>
+    </button>
   )
 }
 
